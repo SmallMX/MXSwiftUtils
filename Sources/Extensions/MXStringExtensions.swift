12f51c6d63809fa509f4296b8b89b28c5668ed44
lmx <854356783@qq.com>
2022-02-11 17:39:18 +0800

    调整

--- Sources/Extensions/MXStringExtensions.swift
+++ Sources/Extensions/MXStringExtensions.swift
@@ -9,24 +9,24 @@ import Foundation
 
 public extension String {
     ///文字宽
-    func mx_widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
+    public func mx_widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
         let font = UIFont.systemFont(ofSize: fontSize)
         let rect = NSString(string: self).boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: height), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(rect.width)
     }
     ///文字高
-    func mx_heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
+    public func mx_heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
         let font = UIFont.systemFont(ofSize: fontSize)
         return self.wj_heightForComment(font: font, width: width)
     }
     
     ///文字高
-    func mx_heightForComment(font: UIFont, width: CGFloat) -> CGFloat {
+    public func mx_heightForComment(font: UIFont, width: CGFloat) -> CGFloat {
         let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(rect.height)
     }
     
-    func mx_heightForComment(fontSize: CGFloat, width: CGFloat, maxHeight: CGFloat) -> CGFloat {
+    public func mx_heightForComment(fontSize: CGFloat, width: CGFloat, maxHeight: CGFloat) -> CGFloat {
         let font = UIFont.systemFont(ofSize: fontSize)
         let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(rect.height)>maxHeight ? maxHeight : ceil(rect.height)
