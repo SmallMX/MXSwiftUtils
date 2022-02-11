0cfa458bdc7f87da0a0f8ee28af5db2b1dfdfd0e
lmx <854356783@qq.com>
2022-02-11 17:32:40 +0800

    string

---
+++ Sources/Extensions/MXStringExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by 刘明昕 on 2022/2/11.
+//
+
+import Foundation
+
+public extension String {
+    ///文字宽
+    func mx_widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: height), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(rect.width)
+    }
+    ///文字高
+    func mx_heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        return self.wj_heightForComment(font: font, width: width)
+    }
+    
+    ///文字高
+    func mx_heightForComment(font: UIFont, width: CGFloat) -> CGFloat {
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(rect.height)
+    }
+    
+    func mx_heightForComment(fontSize: CGFloat, width: CGFloat, maxHeight: CGFloat) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(rect.height)>maxHeight ? maxHeight : ceil(rect.height)
+    }
+}
