191322c4016f0e4853d25a3ba8a3c5e3b3bda601
lmx <854356783@qq.com>
2022-02-11 17:13:44 +0800

    调整

--- Sources/MXSwiftUtils/MXSwiftUtils.swift
+++ Sources/MXSwiftUtils/MXSwiftUtils.swift
@@ -4,7 +4,7 @@ public struct MXSwiftUtils {
     var text = "Hello, World!"
     
     /****跳转到应用系统设置 *****/
-    static func openSystemSetup() {
+    public static func openSystemSetup() {
         let url = URL(string: UIApplication.openSettingsURLString)
         if UIApplication.shared.canOpenURL(url!) {
             UIApplication.shared.open(url!, options: Dictionary(), completionHandler: nil)
@@ -13,27 +13,27 @@ public struct MXSwiftUtils {
     
     /**************获取设备信息**************/
     
-    static func getAppVersion() -> String {
+    public static func getAppVersion() -> String {
         return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
     }
     
-    static func getSystemVersion() -> String {
+    public static func getSystemVersion() -> String {
         return UIDevice.current.systemVersion
     }
     
-    static func getSystemModel() -> String {
+    public static func getSystemModel() -> String {
         return UIDevice.current.model
     }
     
     ///手机号正则匹配
-    static func verifyPhoneNumber(phone: String) -> Bool {
+    public static func verifyPhoneNumber(phone: String) -> Bool {
         let phoneRegex: String = "^(1[3-9])\\d{9}$"
         let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
         return phoneTest.evaluate(with: phone)
     }
     
     /// 检测字符串是否为空
-    static func stringIsBlank(string: String?) -> Bool{
+    public static func stringIsBlank(string: String?) -> Bool{
         if let str = string {
             if str.count > 0 {
                 return false
