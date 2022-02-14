641d46055a5439b98708bc0e452d01cc84178a29
lmx <854356783@qq.com>
2022-02-14 10:58:20 +0800

    1

--- Sources/Extensions/String+MX.swift
+++ Sources/Extensions/String+MX.swift
@@ -5,28 +5,28 @@
 //  Created by 刘明昕 on 2022/2/14.
 //
 
-import Foundation
+import UIKit
 
 public extension String {
     ///文字宽
-    public func mx_widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
+    func mx_widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
         let font = UIFont.systemFont(ofSize: fontSize)
         let rect = NSString(string: self).boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: height), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(rect.width)
     }
     ///文字高
-    public func mx_heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
+    func mx_heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
         let font = UIFont.systemFont(ofSize: fontSize)
-        return self.wj_heightForComment(font: font, width: width)
+        return self.mx_heightForComment(font: font, width: width)
     }
     
     ///文字高
-    public func mx_heightForComment(font: UIFont, width: CGFloat) -> CGFloat {
+    func mx_heightForComment(font: UIFont, width: CGFloat) -> CGFloat {
         let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(rect.height)
     }
     
-    public func mx_heightForComment(fontSize: CGFloat, width: CGFloat, maxHeight: CGFloat) -> CGFloat {
+    func mx_heightForComment(fontSize: CGFloat, width: CGFloat, maxHeight: CGFloat) -> CGFloat {
         let font = UIFont.systemFont(ofSize: fontSize)
         let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(rect.height)>maxHeight ? maxHeight : ceil(rect.height)
